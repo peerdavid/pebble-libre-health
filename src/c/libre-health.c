b26c95e9f9ee2ee709c8d11c0a0236c503898b45
David Peer <david.peer.1991@gmail.com>
2026-01-12 18:28:02 +0100

    Up down and select button implemented

--- src/c/libre-health.c
+++ src/c/libre-health.c
@@ -2,7 +2,9 @@
 
 static Window *s_main_window;
 static TextLayer *s_text_layer;
+static TextLayer *s_status_layer;
 static bool s_launched_by_wakeup = false;
+static bool s_wakeup_enabled = false;
 
 // Define message keys
 #define KEY_STEP_COUNT 100
@@ -11,6 +13,9 @@ static bool s_launched_by_wakeup = false;
 
 // Define Wakeup constants
 #define WAKEUP_COOKIE 717  // A random identifier for your wakeup events
+
+// Persistent storage key
+#define PERSIST_KEY_WAKEUP_ENABLED 1
 
 static void schedule_next_wakeup();
 
@@ -52,27 +57,31 @@ static void send_message_to_phone() {
 }
 
 // --- Wakeup Logic ---
+static void update_status_display() {
+  if (s_status_layer) {
+    if (s_wakeup_enabled) {
+      text_layer_set_text(s_status_layer, "Hourly: ON");
+    } else {
+      text_layer_set_text(s_status_layer, "Hourly: OFF");
+    }
+  }
+}
 
 static void schedule_next_wakeup() {
-  // Check if we already have a wakeup scheduled to avoid duplicates
-  WakeupId id = 0;
-  time_t timestamp = 0;
-  
-  // wakeup_query checks if a wakeup with our specific cookie exists
-  if (wakeup_query(WAKEUP_COOKIE, &timestamp)) {
-    // Already scheduled, no need to do anything
-    // APP_LOG(APP_LOG_LEVEL_DEBUG, "Wakeup already scheduled for %d", (int)timestamp);
+  if (!s_wakeup_enabled) {
+    wakeup_cancel_all();
+    APP_LOG(APP_LOG_LEVEL_INFO, "Wakeup disabled, canceling all wakeups");
     return;
   }
 
-  time_t future_time = time(NULL) +  60 * 60; // One Hour from now
+  wakeup_cancel_all();
   
-  // Schedule the wakeup
-  // notify_if_missed=true will alert the user if the watch was off during the time
-  id = wakeup_schedule(future_time, WAKEUP_COOKIE, true);
+  time_t future_time = time(NULL) + 15; //60 * 60; // One Hour from now
+  
+  WakeupId id = wakeup_schedule(future_time, WAKEUP_COOKIE, true);
   
   if (id >= 0) {
-    APP_LOG(APP_LOG_LEVEL_INFO, ">> NEXT WAKEUP SCHEDULED for: %d (in 60s)", (int)future_time);
+    APP_LOG(APP_LOG_LEVEL_INFO, ">> NEXT WAKEUP SCHEDULED for: %d (in 3600s)", (int)future_time);
   } else {
     APP_LOG(APP_LOG_LEVEL_ERROR, "FAILED to schedule wakeup: %d", (int)id);
   }
@@ -91,11 +100,31 @@ static void wakeup_handler(WakeupId id, int32_t cookie) {
 static void select_click_handler(ClickRecognizerRef recognizer, void *context) {
   text_layer_set_text(s_text_layer, "Sending health\ndata...");
   send_message_to_phone();
-  schedule_next_wakeup(); // Ensure scheduling is active on manual click too
+  schedule_next_wakeup();
+}
+
+static void up_click_handler(ClickRecognizerRef recognizer, void *context) {
+  s_wakeup_enabled = true;
+  persist_write_bool(PERSIST_KEY_WAKEUP_ENABLED, true);
+  text_layer_set_text(s_text_layer, "Hourly wakeup\nACTIVATED");
+  update_status_display();
+  schedule_next_wakeup();
+  APP_LOG(APP_LOG_LEVEL_INFO, "Hourly wakeup ENABLED by user");
+}
+
+static void down_click_handler(ClickRecognizerRef recognizer, void *context) {
+  s_wakeup_enabled = false;
+  persist_write_bool(PERSIST_KEY_WAKEUP_ENABLED, false);
+  text_layer_set_text(s_text_layer, "Hourly wakeup\nDISABLED");
+  update_status_display();
+  schedule_next_wakeup(); // This will cancel all wakeups
+  APP_LOG(APP_LOG_LEVEL_INFO, "Hourly wakeup DISABLED by user");
 }
 
 static void click_config_provider(void *context) {
   window_single_click_subscribe(BUTTON_ID_SELECT, select_click_handler);
+  window_single_click_subscribe(BUTTON_ID_UP, up_click_handler);
+  window_single_click_subscribe(BUTTON_ID_DOWN, down_click_handler);
 }
 
 static void outbox_sent_handler(DictionaryIterator *iterator, void *context) {
@@ -124,29 +153,44 @@ static void main_window_load(Window *window) {
   Layer *window_layer = window_get_root_layer(window);
   GRect bounds = layer_get_bounds(window_layer);
   
-  s_text_layer = text_layer_create(GRect(0, bounds.size.h / 2 - 40, bounds.size.w, 80));
-  text_layer_set_text(s_text_layer, "LibreHealth\nActive\nSELECT to trigger");
+  // Status layer at the top
+  s_status_layer = text_layer_create(GRect(0, 10, bounds.size.w, 30));
+  text_layer_set_text_alignment(s_status_layer, GTextAlignmentCenter);
+  text_layer_set_font(s_status_layer, fonts_get_system_font(FONT_KEY_GOTHIC_18_BOLD));
+  layer_add_child(window_layer, text_layer_get_layer(s_status_layer));
+  update_status_display();
+  
+  // Main text layer in the center
+  s_text_layer = text_layer_create(GRect(0, bounds.size.h / 2 - 40, bounds.size.w, 120));
+  text_layer_set_text(s_text_layer, "LibreHealth\n\nUP: Enable\nSELECT: Send\nDOWN: Disable");
   text_layer_set_text_alignment(s_text_layer, GTextAlignmentCenter);
-  text_layer_set_font(s_text_layer, fonts_get_system_font(FONT_KEY_GOTHIC_24_BOLD));
+  text_layer_set_font(s_text_layer, fonts_get_system_font(FONT_KEY_GOTHIC_18_BOLD));
   layer_add_child(window_layer, text_layer_get_layer(s_text_layer));
 }
 
 static void main_window_unload(Window *window) {
   text_layer_destroy(s_text_layer);
+  text_layer_destroy(s_status_layer);
 }
 
 static void init() {
-  // 1. Open AppMessage
+  // 1. Load persistent state
+  s_wakeup_enabled = persist_exists(PERSIST_KEY_WAKEUP_ENABLED) 
+                     ? persist_read_bool(PERSIST_KEY_WAKEUP_ENABLED) 
+                     : false;
+  APP_LOG(APP_LOG_LEVEL_INFO, "Loaded wakeup state: %s", s_wakeup_enabled ? "ENABLED" : "DISABLED");
+
+  // 2. Open AppMessage
   app_message_register_inbox_received(inbox_received_handler);
   app_message_register_inbox_dropped(inbox_dropped_handler);
   app_message_register_outbox_sent(outbox_sent_handler);
   app_message_register_outbox_failed(outbox_failed_handler);
   app_message_open(128, 128);
 
-  // 2. Subscribe to Wakeup Service (for when app is already open)
+  // 3. Subscribe to Wakeup Service (for when app is already open)
   wakeup_service_subscribe(wakeup_handler);
 
-  // 3. Create Window
+  // 4. Create Window
   s_main_window = window_create();
   window_set_click_config_provider(s_main_window, click_config_provider);
   window_set_window_handlers(s_main_window, (WindowHandlers) {
@@ -155,7 +199,7 @@ static void init() {
   });
   window_stack_push(s_main_window, true);
 
-  // 4. Handle Launch Reason
+  // 5. Handle Launch Reason
   if (launch_reason() == APP_LAUNCH_WAKEUP) {
     // The app was started by the system because the timer fired
     WakeupId id = 0;
@@ -179,7 +223,7 @@ static void init() {
     // The app was started by the user manually
     APP_LOG(APP_LOG_LEVEL_INFO, ">> APP LAUNCHED MANUALLY");
     
-    // Start the loop
+    // Ensure wakeup state is correct
     schedule_next_wakeup();
   }
 }
